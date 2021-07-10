5a580a4efb67567bd216d8a02404d9c3400dfa26
Phudal <080_075@naver.com>
2021-07-10 19:44:32 +0900

    07-10 commit

--- Source/MyActionRPG/Actor/Character/PlayerCharacter/PlayerCharacter.cpp
+++ Source/MyActionRPG/Actor/Character/PlayerCharacter/PlayerCharacter.cpp
@@ -5,6 +5,15 @@
 
 APlayerCharacter::APlayerCharacter()
 {
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_TEST_MESH(
+		TEXT("SkeletalMesh'/Game/Resources/PlayerCharacter/GKnight/Meshes/SK_GothicKnight_VF.SK_GothicKnight_VF'"));
+
+	if (SK_TEST_MESH.Succeeded())
+		GetMesh()->SetSkeletalMesh(SK_TEST_MESH.Object);
+
+	static ConstructorHelpers::FClassFinder<UPlayerCharacterAnimInst> BP_PLAYER_CHARACTER_ANIM_INST(
+		TEXT("AnimBlueprint'/Game/Blueprints/AnimInstance/BP_PlayerCharacter.BP_PlayerCharacter_C'"));
+
 }
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
